b706db83f5229e649860fb25d7c9ee935d703716
mindhackdiva <114305417+d0midigi@users.noreply.github.com>
2025-12-19 13:00:41 -0800

    Delete project-30-faq-accordion directory

--- project-30-faq-accordion/script.js
+++
@@ -1,8 +0,0 @@
-const accordions = document.querySelectorAll(".accordion");
-
-accordions.forEach((accordion) => {
-  accordion.addEventListener("click", () => {
-    const panel = accordion.nextElementSibling;
-    panel.style.display = panel.style.display === "block" ? "none" : "block";
-  });
-});

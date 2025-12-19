2edfb037bbdc26c7e973aefe486aa875f409a880
mindhackdiva <114305417+d0midigi@users.noreply.github.com>
2025-12-19 13:11:00 -0800

    Delete project-82-product-comparison-table directory

--- project-82-product-comparison-table/script.js
+++
@@ -1,8 +0,0 @@
-const rows = document.querySelectorAll("tbody tr");
-
-rows.forEach((row) => {
-  row.addEventListener("click", () => {
-    rows.forEach((r) => r.classList.remove("highlight"));
-    row.classList.add("highlight");
-  });
-});

b1ee1a71974c522c4fc9436f7e2742b40acb61ad
mindhackdiva <114305417+d0midigi@users.noreply.github.com>
2025-12-19 13:12:06 -0800

    Delete project-87-markdown-editor directory

--- project-87-markdown-editor/script.js
+++
@@ -1,5 +0,0 @@
-function updatePreview() {
-  const markdownText = document.getElementById("markdown-input").value;
-  const preview = document.getElementById("preview");
-  preview.innerHTML = marked(markdownText);
-}
